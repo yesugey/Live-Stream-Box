aea407886ad700a4246f07e408ac82bce3826851
Baris Yesugey <baris@fb.com>
2012-02-01 14:44:05 +0000

    first commit ins econd branch

---
+++ index.php
@@ -0,0 +1,16 @@
+<html xmlns:fb="http://ogp.me/ns/fb#">
+	<head>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=155818717865098";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>	</head>
+	<body>
+
+<fb:live-stream event_app_id="155818717865098" width="400" height="500" always_post_to_friends="false"></fb:live-stream>
+
+	</body>
+</html>
